05dda342b48ee57928582144a73dedd5d39818d6
Riday1 <rrriday100@gmail.com>
2022-10-05 09:30:02 +0600

    component $ props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const employes = {
+  backgroundColor: 'lightskyblue',
+  padding: '10px',
+  margin: '10px 0px',
+  borderRadius: '20px',
+  border: '2px solid black'
+}
+const Player = {
+  backgroundColor: 'lightcyan',
+  padding: '10px',
+  margin: '10px 0px',
+  borderRadius: '20px',
+  border: '2px solid black'
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Employee name="kolur bolod" age="34" designation="Marketing Officer"></Employee>
+
+      <Employee name="Kopila shundori" age="30" designation="HR"></Employee>
+      <Players name="Pa-ponda" profession="crickter"></Players>
+      
+
+
+    </div >
   );
 }
+
+
+function Players(props) {
+  console.log(props)
+  return (
+    <div style={Player} className='person'>
+      <h1>Name : {props.name}</h1>
+      <p>professtion : {props.profession}</p>
+    </div>
+  )
+}
+
+
+function Employee(props) {
+  console.log(props)
+  return (
+    <div style={employes} className='emoployes'>
+      <h2>Name: {props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Designation:{props.designation}</p>
+    </div>
+  )
+}
+
+function Friends() {
+  return (
+
+    <div>
+      <h1>Name : shorif</h1>
+      <p>Address : keranigonj</p>
+    </div>
+  )
+}
+
+
 
 export default App;
