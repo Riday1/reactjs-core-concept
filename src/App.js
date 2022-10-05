35f9ca6b5f32de2cd16cc413ab81cd795ba7c8a5
Riday1 <rrriday100@gmail.com>
2022-10-05 19:27:22 +0600

    data loaded into different component by dynamically

--- src/App.js
+++ src/App.js
@@ -12,13 +12,15 @@ const employe = {
 
 
 function App() {
-  const employes = [{ name: 'sofik alam', salary: 20000, address: 'Bonani' }, { name: 'riday hossain', salary: 20000, address: 'khilkhet' }]
+  const employes = [{ name: 'sofik alam', salary: 20000, address: 'Bonani' }, { name: 'riday hossain', salary: 20000, address: 'khilkhet' }, { name: 'sohel', salary: 40000, address: 'Gulshan' }, { name: 'choudhuri', salary: 10000, address: 'Farmgate' }]
   return (
     <div className="App">
 
-      <Employee name={employes[0].name} salary={employes[0].salary}></Employee>
-      <Employee></Employee>
+      <Employee name={employes[0].name} salary={employes[0].salary} address={employes[0].address}></Employee>
 
+      <Employee name={employes[1].name} salary={employes[1].salary} address={employes[1].address}></Employee>
+      <Employee name={employes[2].name} salary={employes[2].salary} address={employes[2].address}></Employee>
+      <Employee name={employes[1].name} salary={employes[1].salary} address={employes[1].address}></Employee>
 
 
     </div >
@@ -29,9 +31,9 @@ function Employee(props) {
 
   return (
     <div style={employe}>
-      <h2>Name :{props.name} </h2>
-      <h5>Salary :{props.salary} </h5>
-      <h5>Address :{props.address}</h5>
+      <h2>Name : {props.name} </h2>
+      <h5>Salary : {props.salary} </h5>
+      <h5>Address : {props.address}</h5>
     </div>
   )
 }
